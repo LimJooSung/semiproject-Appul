45c75acdb3c91607ea84aec81e79d183b6dfeca5
KOSTA <KOSTA@192.168.0.114>
2017-04-11 18:49:28 +0900

    상섭-ㄴ

--- semiproject-practice/src/controller/proposal/board/ProShowContentNoHitController.java
+++ semiproject-practice/src/controller/proposal/board/ProShowContentNoHitController.java
@@ -1,11 +1,15 @@
 package controller.proposal.board;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 import model.board.BoardVO;
 import model.proposal.board.ProBoardDAO;
+import model.proposal.board.ProCommentDAO;
+import model.proposal.board.ProCommentVO;
 
 public class ProShowContentNoHitController implements Controller{
 
@@ -14,6 +18,8 @@ public class ProShowContentNoHitController implements Controller{
 		int boardNo=Integer.parseInt(request.getParameter("boardNo"));		
 		// 개별 게시물 조회  
 		BoardVO vo = ProBoardDAO.getInstance().getProPostingByNo(boardNo);	
+		ArrayList<ProCommentVO> cvo =ProCommentDAO.getInstance().getProPostingCommentList(vo.getBoardNo());
+		request.setAttribute("cvo", cvo);
 		request.setAttribute("bvo", vo);
 		return "board/pro_show_content.jsp";
 	}
